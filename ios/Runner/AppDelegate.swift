0c37736283bb01ce153bbbb8436d30d842a252ca
fordevandbeyond <36726818+fordevandbeyond@users.noreply.github.com>
2022-03-07 00:08:06 +0800

    Update AppDelegate.swift
    IOS google map key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Firebase
 import Flutter
+import GoogleMaps  // Add this import
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyAH4fWM5IbEO0X-Txkm6HNsFAQ3KOfW20I")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
