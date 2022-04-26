88fda29f76aa784d0d5244e6ad3564626c15dcb7
fordevandbeyond <36726818+fordevandbeyond@users.noreply.github.com>
2022-04-26 11:26:34 +0800

    ios change

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import Firebase
 import Flutter
 import GoogleMaps  // Add this import
+import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,19 @@ import GoogleMaps  // Add this import
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+      application.registerForRemoteNotifications()
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
 
     GMSServices.provideAPIKey("AIzaSyAH4fWM5IbEO0X-Txkm6HNsFAQ3KOfW20I")
