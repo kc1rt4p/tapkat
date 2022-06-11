5b36c5d6dd680872c86f00e266d48cbdc3a7da3f
patrick <patlix14@yahoo.com>
2022-06-11 16:54:08 +0800

    TKD - 238, 207 & 244

--- lib/screens/root/root_screen.dart
+++ lib/screens/root/root_screen.dart
@@ -92,7 +92,7 @@ class _RootScreenState extends State<RootScreen> {
   late AuthBloc _authBloc;
   late BarterBloc _barterBloc;
 
-  final _currentVerDate = DateTime(2022, 6, 10, 08);
+  final _currentVerDate = DateTime(2022, 6, 11, 08);
 
   final _appConfig = new LocalStorage('app_config.json');
 
