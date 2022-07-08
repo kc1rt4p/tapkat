90908e458dfa7864a342436d112854f9e55f2cb3
patrick <patlix14@yahoo.com>
2022-07-08 19:00:12 +0800

    fixed issue on barter screen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -36,7 +36,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   List<LocalizationModel> _localizations = [];
   LocalizationModel? _selectedLocalization;
 
-  final _currentVerDate = DateTime(2022, 7, 8, 17);
+  final _currentVerDate = DateTime(2022, 7, 8, 19);
 
   @override
   void initState() {
